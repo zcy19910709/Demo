ab815a2b8b281aaf83773e54a1a42573645c7105
Admin <Admin@DESKTOP-BRUN28K>
2018-02-26 11:44:28 +0800

    add

--- src/main/java/com/zcy/demo/Demo.java
+++ src/main/java/com/zcy/demo/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("demo_zcy");
           System.out.println("master");
-          System.out.println("master");
+          System.out.println("mastera");
 	}
 }
